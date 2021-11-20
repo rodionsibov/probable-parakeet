879905be129be59bdf59a8666d88595085b79406
Rodion Sibov <rodionsibov@gmail.com>
2021-11-20 08:48:31 +0200

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/api/books', (req, res) => {
 app.get('/api/books/:id', (req,res) => {
     console.log(req.params.id);
     const  book = books.find(c => c.id === parseInt(req.params.id))
-    if(!book) res.status(404).send('<strong style="font-family: sans; color: tomato;">Ooops... Cannot find what you are looking for</strong>')
+    if(!book) res.status(404).send('<h1 style="font-family: sans; color: tomato;">Ooops... Cannot find what you are looking for</h1>')
     res.send(book)
 })
 
